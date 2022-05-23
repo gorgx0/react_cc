267151546dd800d801aa9e9fb80d611d6868a68f
Marcin Krasowski <marcin.krasowski@gmail.com>
2022-05-23 21:26:33 +0200

    init

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Header from "./componenets/Header";
+
+const App = () => {
+  return (
+      <div className='container'>
+        <Header title='333' />
+      </div>
+  );
+}
+
+export default App;
